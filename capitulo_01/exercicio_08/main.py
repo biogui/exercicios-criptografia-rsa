85a9f4968fad805c67cbbfef724af520a8ddcd63
biogui <gmrtri@hotmail.com>
2021-08-31 17:36:29 -0300

    Atualizando capítulo 1

--- capitulo_01/exercicio_08/main.py
+++ capitulo_01/exercicio_08/main.py
@@ -3,23 +3,23 @@
  Exercício 8 - Algoritmo de Euclides Estendido.
 '''
 
-UI_BAR = '-' * 50
+BARRA_GRAFICA = '-' * 50
 
 # Realiza o algoritmo de euclides
-def mdc(a: int, b: int) -> int:
+def mdc(a, b):
     return a if b == 0 else mdc(b, a % b)
 
 # Realiza algoritmo de euclides estendido
-def extended_gcd(a : int, b : int, u : int=1, v : int=0, u_: int=0, v_: int=1) -> int: 
+def mdc_extendido(a, b, u, v, u_, v_): 
     if b == 0:
         return a, u, v
     else:
         q  = a // b
         ut = u - q * u_
         vt = v - q * v_
-        return extended_gcd(b, a % b, u_, v_, ut, vt)
+        return mdc_extendido(b, a % b, u_, v_, ut, vt)
 
-def main() -> None:
+def main():
     # Recebe como entrada a e b, da equação ax + by = mdc(a, b)
     inp     = input('Digite a, b e c separados por espaço para a equação ax + by = c\n> ')
     a, b, c = map(int, inp.split())
@@ -28,18 +28,18 @@ def main() -> None:
 
     # Confere se d divide c e printa a solução caso essa seja vazia
     if c % d != 0:
-        print(f'\n{UI_BAR}\n{a}x + {b}y = {c} => S = {{}}\n')
+        print(f'\n{BARRA_GRAFICA}\n{a}x + {b}y = {c} => S = {{}}\n')
         exit()
 
     # Seta as variáveis de a', b' e c' para o cálculo final com mdc estendido
     a_, b_, c_ = a / d, b / d, c / d
 
     # Calcula alpha e beta por euclides estendido
-    _, alpha, beta = extended_gcd(a_, b_)
+    _, alpha, beta = mdc_extendido(a_, b_)
 
     # Seta a solução e printa o resultado
     x, y = c_ * alpha, c_ * beta
-    print(f'\n{UI_BAR}\n{a}x + {b}y = {c} => S = {{{x:.0f}, {y:.0f}}}\n')
+    print(f'\n{BARRA_GRAFICA}\n{a}x + {b}y = {c} => S = {{{x:.0f}, {y:.0f}}}\n')
 
 if __name__ == '__main__':
     main()
--- capitulo_01/exercicio_09/main.py
+++ capitulo_01/exercicio_09/main.py
@@ -2,12 +2,12 @@
  Números Inteiros e Criptografia RSA - Capítulo 1.
  Exercício 9 - Teste de probabilidade para par de co-primos.
 '''
-from math import pi
-from tqdm import tqdm
+from math   import pi
+from tqdm   import tqdm
 from random import randint
 
-UI_BAR         = '-' * 50
-EXPEC_QUOTIENT = 6.0 / pi**2
+BARRA_GRAFICA         = '-' * 50
+QUOCIENTE_ESPERADO    = 6.0 / pi**2
 
 get_rand = lambda: randint(0, 1e30)
 
@@ -16,36 +16,38 @@ def gcd(a, b):
     return a if b == 0 else gcd(b, a % b)
 
 # Executa o teste para diferentes quantides de pares
-def run_test():
-    results   = dict()
-    amts_list = map(int, [1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7])
-    for cur_amt in amts_list:
-        description  = f'Rodando para {cur_amt:8d} pares'
-        amt_equals_1 = 0
-        for _ in tqdm(range(cur_amt), ncols=90, desc=description):
-            amt_equals_1 += (gcd(get_rand(), get_rand()) == 1)
-        
-        results[cur_amt] = amt_equals_1 / cur_amt
+def roda_teste():
+    resultados = dict()
+    qtds       = map(int, [1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7])
+    for qtd_atual in qtds:
+        descricao  = f'Rodando para {qtd_atual:8d} pares'
+        qtd_de_iguais_a_1 = 0
+        for _ in tqdm(range(qtd_atual), ncols=90, desc=descricao):
+            qtd_de_iguais_a_1 += (gcd(get_rand(), get_rand()) == 1)
 
-    return results
+        resultados[qtd_atual] = qtd_de_iguais_a_1 / qtd_atual
 
-def main() -> None:
+    return resultados
+
+def main():
     # Realiza testes para diferentes quantidades de pares
-    results = run_test()
+    resultados = roda_teste()
 
     # Printa os dados do resultado como um todo
-    header=[
-        UI_BAR,
-        f'Quociente esperado = 6/pi² = {EXPEC_QUOTIENT}',
-        UI_BAR
+    cabecalho=[
+        BARRA_GRAFICA,
+        f'Quociente esperado = 6/pi² = {QUOCIENTE_ESPERADO}',
+        BARRA_GRAFICA
     ]
-    print('\n'.join(header))
+    print('\n'.join(cabecalho))
     print(f'Quantidade de pares | Quociente obtido ')
-    for amt_pair, quotient in results.items():
-        print(f' {amt_pair:<18} |  {quotient:<16}')
+    for qtd_pares, quotient in resultados.items():
+        print(f' {qtd_pares:<18} |  {quotient:<16}')
 
-    print('\nAssim, verificamos que quanto maior a quantidade de pares testados ' +
-        'mais próximo de 6/pi² é o quociente.')
+    print(
+        '\nAssim, verificamos que quanto maior a quantidade\n' +
+        'de pares testados mais próximo de 6/pi² é o quociente.'
+    )
 
 if __name__ == '__main__':
     main()
