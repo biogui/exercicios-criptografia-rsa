5ceeee11aa9a32884e24eea58d6ab99e6dd0d0db
biogui <gmrtri@hotmail.com>
2021-08-31 20:00:07 -0300

    Atualizando repo com capítulo 2

---
+++ capitulo_02/exercicio_11/main.py
@@ -0,0 +1,60 @@
+'''
+ Números Inteiros e Criptografia RSA - Capítulo 2.
+ Exercício 11 - Números altamente compostos.
+'''
+from math import sqrt
+from sympy import primerange
+
+# Obtem a fatoração de um inteiro
+def fatora(n):
+    possiveis_fatores = primerange(2, n + 1)
+
+    fatoracao = dict()
+    for fator in possiveis_fatores:
+        if n % fator == 0:
+            fatoracao[fator] = 0
+            while n % fator == 0:
+                fatoracao[fator] += 1
+                n /= fator
+
+    return fatoracao
+
+# Conta número de divisores de um inteiro positivo n
+def conta_divisores(n):
+    raiz_de_n = int(sqrt(n))
+
+    num_de_divs = 0
+    for i in range(1, raiz_de_n + 1):
+        if n % i == 0:
+            num_de_divs += 1 if n / i == i else 2
+
+    return num_de_divs
+
+def main():
+    print('Obtendo todos os números inteiros positivos altamente compostos menores que 5000:')
+    r = 5000
+    qtds_de_divs = [conta_divisores(i + 1) for i in range(r)]
+
+    nums_altamente_compostos = [1]
+    qtd_max_atual = qtds_de_divs[0]
+
+    for n, qtd_de_divs in enumerate(qtds_de_divs):
+        if qtd_de_divs > qtd_max_atual:
+            nums_altamente_compostos.append(n + 1)
+            qtd_max_atual = qtd_de_divs
+
+    print(nums_altamente_compostos[0])
+    for n in nums_altamente_compostos[1:]:
+        fatores = [f'{p}^{e}' if e != 1 else str(p) for p, e in fatora(n).items()]
+        print(f'{n} = {(' * ').join(fatores)}')
+
+    print(
+        '\nObservamos algumas coisas nessas dessa lista:\n' +
+        '\t- Todos números altamentes compostos (exceto o 1) são pares, justificado abaixo;\n' +
+        '\t- Numa fatoração aparecem todos os primos menores que o maior primo dessa fatoração;\n' +
+        '\t- Nas fatorações as multiplicidades de cada primo aparecem de forma decrescente\n' +
+        '\t  (para p_1^e_1 e p_2^e_2, temos p_1 < p_2 ==> e_1 >= e_2).\n'
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ capitulo_02/exercicio_12/main.py
@@ -0,0 +1,37 @@
+'''
+ Números Inteiros e Criptografia RSA - Capítulo 2.
+ Exercício 12 - Fatoração por Algoritmo de Fermat.
+'''
+from math import sqrt
+
+# Obtém dois fatores de um número n utilizando o algoritmo de Fermat
+def fatores_por_fermat(n):
+    if n % 2 == 0: return (2, n//2)
+
+    # Calcula o x inicial
+    x = int(sqrt(n))
+
+    if x**2 == n: return (x, x)
+
+    def nova_tentativa(x):
+        x += 1
+        if x == (n + 1) / 2: return (1, n)
+
+        y = sqrt(x**2 - n)
+        y_int = int(y)
+
+        return (x + y_int, x - y_int) if y == y_int else nova_tentativa(x)
+
+    return nova_tentativa(x)
+
+def main():
+    print('Digite um inteiro n menor que 2^32, para obter dois fatores pela fatoração de Fermat:')
+    n = int(input('> '))
+
+    fator_1, fator_2 = fatores_por_fermat(n)
+    resultado = f'n = {n} é primo' if fator_1 == 1 else f'n = {n} = {fator_1} * {fator_2}'
+
+    print(f'\nO resultado após a fatoração foi: {resultado}')
+
+if __name__ == '__main__':
+    main()
